a310a2498144ebbbf0b9dba40355cd51cd6d3984
Xander Norwood <xandersody@gmail.com>
2026-04-20 11:27:51 -0400

    removed line from treesitter.nix

--- config/plugins/kickstart/treesitter.nix
+++ config/plugins/kickstart/treesitter.nix
@@ -84,9 +84,6 @@
 
       indent = {
         enable = true;
-        disable = [
-          "ruby"
-        ];
       };
 
       # There are additional nvim-treesitter modules that you can use to interact
