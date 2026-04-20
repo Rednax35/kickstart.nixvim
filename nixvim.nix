9fe92e1818657196079f87829473c16ea05176ab
Xander Norwood <xandersody@gmail.com>
2026-04-20 11:34:47 -0400

    catppuccin

--- nixvim.nix
+++ nixvim.nix
@@ -119,18 +119,14 @@ in
   #
   # If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   colorschemes = {
-    # https://nix-community.github.io/nixvim/colorschemes/tokyonight/index.html
-    tokyonight = {
+    catppuccin = {
       enable = true;
+
       settings = {
-        # Like many other themes, this one has different styles, and you could load
-        # any other, such as 'storm', 'moon', or 'day'.
-        style = "night";
-        styles = {
-          comments = {
-            italic = false; # Disable italics in comments
-          };
-        };
+        flavour = "mocha";
+
+        # Needed to keep terminal transparency, if any
+        transparent_background = false;
       };
     };
   };
