b2934237c2c92d2d9288fcf64c3ecf538ee27b3f
Xander Norwood <xandersody@gmail.com>
2026-04-20 16:41:55 -0400

    Forgot to add thing

---
+++ config/plugins/custom/ui.nix
@@ -0,0 +1,31 @@
+{
+  barbar = {
+    enable = true;
+    keymaps = {
+      next.key = "<TAB>";
+      previous.key = "<S-TAB>";
+      close.key = "<C-w>";
+    };
+  };
+
+  undotree = {
+    enable = true;
+    settings = {
+      autoOpenDiff = true;
+      focusOnToggle = true;
+    };
+  };
+
+  notify = {
+    enable = true;
+  };
+
+  nui = {
+    enable = true;
+  };
+
+  noice = {
+    enable = true;
+  };
+
+}
